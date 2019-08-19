90d0021e15ac41c2bd205542841d86493a55c9a0
Anuar Otynshin <strigeandro@gmail.com>
2019-08-19 19:51:27 +0600

    initialize the chess board

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,26 @@
 package main.java;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        char[][] board = new char[8][8];
+
+        printBoard(board);
+    }
+
+    public static void printBoard(char[][] board) {
+        System.out.println("   1  2  3  4  5  6  7  8");
+        for (char r = 'A'; r <= 'H'; r++) {
+            System.out.print(r + "| ");
+
+            for (int c = 0; c < 8; c++) {
+                System.out.print("_");
+                System.out.print("  ");
+            }
+
+            System.out.println("|" + r);
+        }
+        System.out.println("   1  2  3  4  5  6  7  8");
     }
 }
